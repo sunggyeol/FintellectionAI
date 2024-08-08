6d744f5de20ca23372766696f4a12f9bbf8dffe9
sunggyeol <sunggyeol@vt.edu>
2024-08-08 21:18:23 +0900

    Update models list

--- src/frontend/src/stores/slices/configSlice.ts
+++ src/frontend/src/stores/slices/configSlice.ts
@@ -22,7 +22,7 @@ export const createConfigSlice: StateCreator<
   [],
   ConfigStore
 > = (set) => ({
-  model: ChatModel.GPT_3_5_TURBO,
+  model: ChatModel.GPT_4O_MINI,
   localMode: false,
   proMode: false,
   setModel: (model: ChatModel) => set({ model }),
@@ -35,7 +35,7 @@ export const createConfigSlice: StateCreator<
 
       const newLocalMode = !state.localMode;
       const newModel = newLocalMode
-        ? ChatModel.LLAMA3
+        ? ChatModel.LLAMA_3_1_405B
         : ChatModel.GPT_4O_MINI;
       return { localMode: newLocalMode, model: newModel };
     }),
